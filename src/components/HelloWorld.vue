4794bc0c28daf8dd3776f1575cd44391086452c0
Bestian Tang <bestian@gmail.com>
2021-04-03 07:43:50 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <h1>草山共學堂</h1>
     <p>我們在此做生命經驗回溯、書寫、繪畫、唱歌舞蹈，或劉慧君老師的食氣、金錢課提到的部份練習。</p>
     <p>大人先快樂的自主學習，把身心底氣建穩固，孩子也會一起共振，體會1+1大於2的共創：）</p>
-    <a href="https://forms.gle/ggFsGCKBN9yrdkDK8" rel="noopener noreferrer">詳情與報名表請按此</a>
+    <a href="https://forms.gle/ggFsGCKBN9yrdkDK8" target="_blank" rel="noopener noreferrer">詳情與報名表請按此</a>
   </div>
 </template>
 
