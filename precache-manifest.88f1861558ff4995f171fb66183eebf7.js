e25ffc474d8fc41a1fc4612850773f66cb35ab77
Bestian Tang <bestian@gmail.com>
2021-04-03 07:44:15 +0800

    deploy

---
+++ precache-manifest.88f1861558ff4995f171fb66183eebf7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad68c583168530d1730",
+    "url": "/group/css/app.8562f9b6.css"
+  },
+  {
+    "revision": "1a78b71b9768471b6933",
+    "url": "/group/css/chunk-1b97847c.00fb25cd.css"
+  },
+  {
+    "revision": "51cf774015f50de9cd03",
+    "url": "/group/css/chunk-7616c3d0.66954b81.css"
+  },
+  {
+    "revision": "db5f27153e19c22901dcadfe70afb555",
+    "url": "/group/img/main.db5f2715.jpg"
+  },
+  {
+    "revision": "83ed32c84076ffe754510ee83e4a4a6b",
+    "url": "/group/img/main2.83ed32c8.jpg"
+  },
+  {
+    "revision": "0b69d85b4d87c882e3ae976d1f1fcb7d",
+    "url": "/group/index.html"
+  },
+  {
+    "revision": "0ad68c583168530d1730",
+    "url": "/group/js/app.98655480.js"
+  },
+  {
+    "revision": "1a78b71b9768471b6933",
+    "url": "/group/js/chunk-1b97847c.95479c58.js"
+  },
+  {
+    "revision": "51cf774015f50de9cd03",
+    "url": "/group/js/chunk-7616c3d0.1c8f87d9.js"
+  },
+  {
+    "revision": "f077cadcb110dc8da505",
+    "url": "/group/js/chunk-vendors.fc010b69.js"
+  },
+  {
+    "revision": "ce2a0ab72c0810bda713ac61bf1b43d1",
+    "url": "/group/logo.png"
+  },
+  {
+    "revision": "102cc283d71127031f1db35da72646f9",
+    "url": "/group/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/group/robots.txt"
+  }
+]);
